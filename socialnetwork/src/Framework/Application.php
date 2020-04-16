f7c44c720d837410396c853d6a8f85eba9c0d4bc
Yao Jean-Claude <jeandev84@yahoo.com>
2020-04-16 21:57:13 +0300

    Error Handling Whoops

--- socialnetwork/src/Framework/Application.php
+++ socialnetwork/src/Framework/Application.php
@@ -1,6 +1,9 @@
 <?php
 namespace Framework;
 
+use Whoops\Run as WhoopsRun;
+use Whoops\Handler\PrettyPageHandler as WhoopsPrettyPageHandler;
+
 
 /**
  * Class Application
@@ -8,8 +11,26 @@ namespace Framework;
 */
 class Application
 {
+
+    /**
+     * $ composer search whoops
+     * $ composer require filp/whoops
+    */
     public function run()
     {
-        echo 'method invoked';
+        $this->initWhoops();
+    }
+
+
+    /**
+     * Initialise error handler Whoops
+     * @return $this
+    */
+    public function initWhoops()
+    {
+        $whoops = new WhoopsRun();
+        $handler = new WhoopsPrettyPageHandler();
+        $whoops->pushHandler($handler)->register();
+        return $this;
     }
 }
