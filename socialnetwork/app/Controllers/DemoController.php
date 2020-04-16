25c6e3ef06c7497e92145bd0e549f04929224a84
Yao Jean-Claude <jeandev84@yahoo.com>
2020-04-17 00:56:16 +0300

    DemoController

---
+++ socialnetwork/app/Controllers/DemoController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controllers;
+
+
+/**
+ * Class DemoController
+ * @package App\Controllers
+*/
+class DemoController
+{
+    public function index()
+    {
+        echo __METHOD__;
+    }
+
+    public function show()
+    {
+        echo __METHOD__;
+    }
+}
