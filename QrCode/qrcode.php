7ff689d876107d00d87aba9fcd47ddf7a694b7c7
Yao Jean-Claude <jeandev84@yahoo.com>
2020-04-20 09:20:19 +0300

    Generate Scannable QR Codes

--- QrCode/qrcode.php
+++ QrCode/qrcode.php
@@ -1,2 +1,27 @@
 <?php
+header('Content-Type: image/png');
 
+use Endroid\QrCode\QrCode;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+
+if(isset($_GET['text']))
+{
+    $size =  isset($_GET['size']) ? (int) $_GET['size'] : 200;
+    $padding = isset($_GET['padding']) ? $_GET['padding'] : 10;
+
+    $qr = new QrCode();
+    $qr->setText($_GET['text']);
+    $qr->setSize($size);
+    $qr->setPadding($padding);
+
+    $qr->render();
+}
+
+/*
+$qr = new QrCode();
+$qr->setText('http://www.google.co.uk');
+$qr->setSize(200);
+$qr->setPadding(10);
+*/
